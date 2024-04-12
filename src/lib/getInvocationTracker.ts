89ba21d39cae266f711fadf4d7e4ed9bf26b9d58
Nicolas Bottarini <nicolasbottarini@gmail.com>
2024-04-12 12:05:20 -0300

    improve error message when passed object is not a mock nor a spy

--- src/lib/getInvocationTracker.ts
+++ src/lib/getInvocationTracker.ts
@@ -4,5 +4,5 @@ import { Spy } from '@/spy/Spy'
 export function getInvocationTracker(instance: any) {
     if (instance.__mocktMocker) return (instance.__mocktMocker as Mocker).invocationTracker
     if (instance.__mocktSpy) return (instance.__mocktSpy as Spy<any>).invocationTracker
-    throw new Error('Error')
+    throw new Error('Given instance is not a mock nor a spy')
 }
