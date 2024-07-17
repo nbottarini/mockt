08f75d8a02038330ab08953e124407c53dec4852
Nicolas Bottarini <nicolasbottarini@gmail.com>
2024-07-17 10:14:11 -0300

    minor fix in mockt function typing

--- src/mockt.ts
+++ src/mockt.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-redeclare */
 import { Mocker } from '@/Mocker'
 
+export function mockt<T>(clazz?: (new (...args: any[]) => T)): T
+export function mockt<T>(clazz?: any): T
 export function mockt<T>(clazz?: (new (...args: any[]) => T) | any): T {
     const mocker = new Mocker(clazz)
     return mocker.instance
